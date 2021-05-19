343127f00d4e43d28e23575b86256524aecd0791
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-05-19 21:52:25 +1000

    no longer reinstalling venv in build scripts

--- build.bat
+++ build.bat
@@ -2,15 +2,9 @@
 :: clean build dirs
 rmdir build /s
 rmdir dist /s
-rmdir venv /s
 :: build backend_cli.exe
-py -3.9 -m venv venv
-call venv/scripts/activate
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
 pyinstaller --onefile backend_cli.py
 xcopy /s dist/backend_cli.exe ./backend_cli.exe
-:: replace `"python", ["./backend_cli.py", ` in main.js with `"backend_cli.exe", [`
+:: TODO: replace `"python", ["./backend_cli.py", ` in main.js with `"backend_cli.exe", [`
 :: package electron app
-:: TODO: compress assets
 electron-packager . pea --platform=win32 --arch=x64 --icon=./assets/pea.png
--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 ## This bash script prepares the python backend
 # clean build dirs
-rm -r build dist venv
+rm -r build dist
 # build backend_cli
-py -3.9 -m venv venv
-call venv/scripts/activate
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
 pyinstaller --onefile backend_cli.py
 cp -r dist/backend_cli* ./
 # TODO: retarget main.js from backend_cli.py to backend_cli
-# replace `"python", ["./backend_cli.py", ` in main.js with `"backend_cli.exe", [`
+# TODO: replace `"python", ["./backend_cli.py", ` in main.js with `"backend_cli.exe", [`
 # package electron app
-# TODO: compress assets
 electron-packager . pea --platform=linux --arch=x64 --icon=./assets/pea.png
